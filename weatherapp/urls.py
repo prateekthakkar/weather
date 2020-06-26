0bf42876b1bab0d3642db3ca5f3fcb9ef6e37991
Prateek <Pratikthakkar790@gmail.com>
2020-06-26 14:21:47 +0530

    add new validations

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.Home,name="Home"),
+    path('delete/<city_name>', views.delete_city,name="delete_city"),
+
 ]
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,33 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from .models import *
 from .forms import CityForm 
+from django.contrib import messages
 # Create your views here.
 
 def Home(request):
-    url  = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=Your Open Weather Api Key'
+    url  = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=You Api Key'
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+            
+            if existing_city_count == 0:  
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    messages.error(request, 'city does not exsist!')
+            else:
+                messages.warning(request, 'city already exsist!')
 
     form = CityForm()
-
     cities = City.objects.all()
-
     weather_data = []
 
     for city in cities:
-
         r = requests.get(url.format(city)).json()
-
         city_weather = {
             'city' : city.name,
             'temperature' : r['main']['temp'],
             'description' : r['weather'][0]['description'],
             'icon' : r['weather'][0]['icon'],
         }
-
         weather_data.append(city_weather)
 
     context = {'weather_data' : weather_data, 'form' : form}
     return render(request,'weatherapp/weather.html',context)
+
+def delete_city(request,city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect("Home")
